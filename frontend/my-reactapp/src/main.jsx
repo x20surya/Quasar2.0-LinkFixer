4566b9ed875e27cfa6c743384c6405e90d85e7ed
utkarsh-devdev <149830536+utkarsh-devdev@users.noreply.github.com>
2025-03-29 16:09:02 +0530

    sign in sign up create

--- frontend/my-reactapp/src/main.jsx
+++ frontend/my-reactapp/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import SignIn from './Auth/SignIn'
+import SignUp from './Auth/SignUP'
 import App from './App.jsx'
 import {
   createBrowserRouter,
@@ -8,7 +10,9 @@ import {
 } from "react-router-dom";
 
 const router = createBrowserRouter([
-  {path:"/",element:<App/>}
+  {path:"/",element:<App/>},
+  {path:"/signin",element:<SignIn/>},
+  {path:"/signup",element:<SignUp/>}
 ])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
