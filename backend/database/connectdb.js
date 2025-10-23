d775e71948fde22269719f751c09c032fd02872e
Aakrisht Srivastava <aakrisri55@gmail.com>
2025-10-23 16:39:01 +0530

    Add .env.example

--- backend/database/connectdb.js
+++ backend/database/connectdb.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 export const connectDB=async()=>{
     try{
-        const connection=await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected: ${connection.connection.host}`);
-
+        const connection=await mongoose.connect(process.env.MONGO_URI, {
+            dbName : "linkfixerDB"
+        })
+        console.log(`MongoDB connected: ${connection.connection.host}`)
     }
     catch(error){
         console.log(`Error: ${error.message}`);
