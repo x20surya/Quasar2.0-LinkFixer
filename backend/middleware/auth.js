7cf43a6efb80ae420f1132d316806a60b52c5622
Aakrisht Srivastava <aakrisri55@gmail.com>
2025-10-26 19:19:55 +0530

    CHanges

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -9,7 +9,6 @@ export const auth = (req, res, next) => {
   if (!token) {
     return res.status(401).json({ msg: "No token, authorization denied" });
   }
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
