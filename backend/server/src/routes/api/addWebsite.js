2d05120102bbb3e16fe77b1a22faecfcd88331f7
Aakrisht Srivastava <aakrisri55@gmail.com>
2025-11-18 15:37:43 +0530

    Add sitemapLink push to addWebsite

--- backend/server/src/routes/api/addWebsite.js
+++ backend/server/src/routes/api/addWebsite.js
@@ -1,43 +1,75 @@
 import { auth } from "../../middleware/auth.js";
 import { User, Website } from "../../models/user.js";
-import { addToQueue } from "../../utils/scheduler/logger.js";
-import {Router} from "express";
+import { Router } from "express";
+import { parseSitemap } from "../../utils/website/sitemap.js";
+import enqueue from "../../utils/scheduler/enqueue.js";
 
 const router = Router();
 router.post("/addWebsite", auth, async (req, res) => {
-  const { startURL } = req.body;
+  let { link } = req.body;
   const user = await User.findById(req.user.id)
     .select("-password")
     .populate("websites");
   if (!user) {
-    return res.status(404).json({ msg: "User not found" });
+    return res.status(404).json({ error: "User not found" });
   }
-  if (!startURL) {
-    return res.status(400).json({ msg: "Please provide a start URL" });
+  if (!domain.includes("http")) {
+    domain = "http://" + domain
+  }
+  if (!link) {
+    return res.status(400).json({ error: "Please provide a start URL" });
+  }
+  const url = new URL(domain)
+
+  const domain = url.hostname
+  if (!domain) {
+    return res.status(400).json({ error: "Please provide a start URL" });
   }
 
-  if (user.websites.some((website) => website.startURL === startURL)) {
-    return res.status(400).json({ msg: "Website already exists" });
+  if (user.websites.some((website) => website.domain === domain)) {
+    return res.status(400).json({ error: "Website already exists" });
   }
-  if (!startURL.startsWith("http://") && !startURL.startsWith("https://")) {
-    return res.status(400).json({
-      msg: "Please provide a valid URL strting with http:// or https://",
-    });
-  }
+  let reports = {}
 
+  const sitemapURL = url.hostname + "/sitemap.xml"
+
+  const sitemapLinks = await parseSitemap(sitemapURL)
+
+  if (sitemapLinks.length === 0) {
+    reports.sitemap = {
+      error: `No links detected at ${sitemapURL}`
+    }
+  } else {
+    reports.sitemap = {
+      url: sitemapURL,
+      links: sitemapLinks
+    }
+  }
   try {
     const website = new Website({
-      startURL,
+      domain,
       userID: req.user.id,
+      checkedLinks: [],
+      sitemapLinks: (sitemapLinks.length === 0) ? [link] : sitemapLinks
     });
-    await website.save();
     user.websites.push({
       id: website._id,
-      startURL: website.startURL,
+      domain: website.domain,
     });
-    await user.save();
-    addToQueue(website.id)
-    return res.status(201).json(website);
+
+    try {
+      await Promise.all([user.save(), website.save()])
+    } catch (err) {
+      console.log(`Error in saving data to db in addWebsite`)
+      console.log(err)
+    }
+    await enqueue("priority_high_domain", domain)
+
+    return res.status(201).json({
+      ...res,
+      msg: `Website added sucessfully`,
+      website: website
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ msg: "Server error" });
