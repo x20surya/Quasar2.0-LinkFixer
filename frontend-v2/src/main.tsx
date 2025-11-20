79fc008e1c7a6c4d1e5ce74f3ceb61ec40adb9c0
Aakrisht Srivastava <aakrisri55@gmail.com>
2025-11-21 00:33:04 +0530

    Implement React router

--- frontend-v2/src/main.tsx
+++ frontend-v2/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router"
+import "./index.css"
+import App from "./App.tsx"
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/app",
+    element: <App />,
+  },
+])
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
