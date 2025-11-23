6b647d257d7a1c82959c42300e7e5da5d0d7b2ac
Aakrisht Srivastava <aakrisri55@gmail.com>
2025-11-23 18:51:37 +0530

    Add Login Route

--- frontend-v2/src/main.tsx
+++ frontend-v2/src/main.tsx
@@ -3,12 +3,21 @@ import { createRoot } from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router"
 import "./index.css"
 import App from "./App.tsx"
+import Home from "./pages/Home/Home.tsx"
+import Login from "./pages/Login/Login.tsx"
 
 const router = createBrowserRouter([
   {
     path: "/app",
     element: <App />,
   },
+  {
+    path : "/",
+    element : <Home/>
+  }, {
+    path : "/login",
+    element : <Login/>
+  }
 ])
 
 createRoot(document.getElementById("root")!).render(
